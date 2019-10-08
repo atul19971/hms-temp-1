df1043a8898d4187790c1b092cf5b241ce89d0d4
atulsingh <atulsingh@atuls-air>
2019-10-07 20:42:52 -0400

    TestController added

---
+++ hms/api/src/main/java/com/dishtavar/hms/api/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.dishtavar.hms.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+//	@GetMapping(value = "/{id}")
+//    public Foo findById(@PathVariable("id") Long id) {
+//        return RestPreconditions.checkFound(service.findById(id));
+//    }
+// 
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Long create(@RequestBody Foo resource) {
+//        Preconditions.checkNotNull(resource);
+//        return service.create(resource);
+//    }
+// 
+//    @PutMapping(value = "/{id}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public void update(@PathVariable( "id" ) Long id, @RequestBody Foo resource) {
+//        Preconditions.checkNotNull(resource);
+//        RestPreconditions.checkNotNull(service.getById(resource.getId()));
+//        service.update(resource);
+//    }
+// 
+//    @DeleteMapping(value = "/{id}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public void delete(@PathVariable("id") Long id) {
+//        service.deleteById(id);
+//    }
+}
